013d345b2e59e9f2f23551a43405da5c1889ac09
Zarak Khan <zarak.khan@outlook.com>
2026-08-14 10:46:03 -0400

    completed send_exercises

--- backend/app.py
+++ backend/app.py
@@ -15,11 +15,37 @@ def get_exercises():
     cursor.execute("SELECT * FROM exercise")
     exercises = cursor.fetchall()
 
+    # close
     connection.close()
 
     return jsonify(exercises)
 
 # now for post
+@app.route('/api/exercises', methods=['POST'])
+def send_exercises():
+
+    # create data variable for reading json using our imported request
+    data = request.get_json()
+
+    # read name and muscle_group from data
+    name = data["name"]
+    muscle_group = data["muscle_group"]
+
+    connection = get_db_connection()
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        INSERT INTO exercise (name, muscle_group)
+        VALUES (?, ?)
+    """, (name, muscle_group))
+
+    # commit and close
+    connection.commit()
+    connection.close()
+
+    # we add 201 because new resource created using status code 201
+    return jsonify(data), 201
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
