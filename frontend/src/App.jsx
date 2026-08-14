2f06d8cd57dcb9c858f56c3be198fa2599a49b94
Zarak Khan <zarak.khan@outlook.com>
2026-08-14 12:42:27 -0400

    created button to add exercise with name and muscle group

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from 'react'
 
 function App() {
+  // follows format of currentValue, wayToChangeIt, useState(initialValue)
   const [exercises, setExercises] = useState([])
+  const [name, setName] = useState('')
+  const [muscleGroup, setMuscleGroup] = useState('')
 
   useEffect(() => {
     fetch('http://127.0.0.1:5000/api/exercises')
@@ -9,11 +12,48 @@ function App() {
       .then(data => setExercises(data))
   }, [])
 
+  // function to add exercise and muscle group, uses our post function we defined in backend app.py
+  // we use this because we want to send flask some new data, i.e. REACT -> FLASK
+  const addExercise = () => {
+  fetch('http://127.0.0.1:5000/api/exercises', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name: name,
+      muscle_group: muscleGroup,
+    }),
+  })
+}
+
   return (
     <div>
       <h1>Workout Progression Tracker</h1>
 
       <h2>Exercises</h2>
+
+       {/* inputs are controlled components, so their values come from state, 
+       and the onChange handler updates that state whenever the user types. */}
+      <div>
+        <input
+          type="text"
+          placeholder="Exercise name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <input
+          type="text"
+          placeholder="Muscle group"
+          value={muscleGroup}
+          onChange={(e) => setMuscleGroup(e.target.value)}
+        />
+
+        {/* button click for adding exercise */}
+        <button onClick={addExercise}>Add Exercise</button>
+
+      </div>
 
       {exercises.map(exercise => (
         <div key={exercise.id}>
