9d663b9cbd1ab43b59b68977a6e7a381ac07812a
Zarak <zarak.khan@outlook.com>
2026-08-20 12:53:05 -0400

    completed update_workout_session (put)
    need to come back home to test properly

--- backend/app.py
+++ backend/app.py
@@ -136,5 +136,32 @@ def send_workout_esssion():
     # we add 201 because new resource created using status code 201
     return jsonify(data), 201
 
+# WORK NOTES!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# put but for workout_session
+@app.route('/api/workout_sessions/<id>', methods=['PUT'])
+def update_workout_session(id):
+
+    # create data variable for reading json using our imported request
+    data = request.get_json()
+
+    # read date from data
+    date = data["date"]
+
+    connection = get_db_connection()
+    cursor = connection.cursor()
+
+    cursor.execute("""
+    UPDATE workout_session
+    SET date = ?
+    WHERE id = ?
+    """, (date, id))
+
+    # commit and close
+    connection.commit()
+    connection.close()
+    
+    # 200 = successful update
+    return jsonify(data), 200
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
